0c8cdc6d9b44c36ee135b763a4fe71a4e44f4893
Simon Schwaiger <simonschwaiger1308@gmail.com>
2022-05-11 12:24:37 +0200

    Add training and model instantiation blueprints

---
+++ app/MTExperiments/TrainingScripts/ModelManipulationBlueprint.py
@@ -0,0 +1,103 @@
+import os
+import sys
+import time
+sys.path.append("/catkin_ws/src/jointcontrol/scripts")
+
+import gym
+import gym_jointcontrol
+from discreteActionWrapper import *
+
+import numpy as np
+import torch as th
+
+from stable_baselines3 import PPO, DQN, DDPG
+from stable_baselines3.common.evaluation import evaluate_policy
+
+
+
+## Instantiate env and model
+model_class = PPO
+
+if model_class == DQN:
+    env = jointcontrolDiscrete(
+        gym.make('jointcontrol-v0', jointidx = 0),
+        discretisation = 0.1
+    )
+else:
+    env = gym.make('jointcontrol-v0', jointidx = 0) 
+
+
+
+env.reset(config={ "initialPos":0, "stepPos":-1.57, "samplesPerStep":150, "maxSteps":40 })
+
+model = model_class("MlpPolicy", env, tensorboard_log="/training_tensorboard/", verbose=1)
+
+
+# Close env
+if model_class == DQN:
+    env.env.env.closeSharedMem()
+else:
+    env.env.closeSharedMem()
+
+
+
+
+
+
+
+## Set network architecture kwargs
+# PPO & DDPG type policy args
+policy_kwargs = dict(
+    activation_fn = th.nn.Tanh,
+    net_arch=[ dict(pi=[ 32,32 ], vf=[ 32,32 ]) ]
+)
+
+# DQN type policy args
+policy_kwargs = dict(
+    activation_fn = th.nn.Tanh,
+    net_arch=[ 32, 32, 32 ]
+)
+
+
+## Change optimiser params
+optimParams = model.policy.optimizer.state_dict()
+optimParams["param_groups"][0]["lr"] = 0.0003
+optimParams["param_groups"][0]["betas"] = (0.9, 0.999)
+model.policy.optimizer.load_state_dict( optimParams )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+## Evaluation episodes with callback
+evalEpisodeRewards = []
+evalEpisodeObservations = []
+def evaluationCallback(localArgs, globalArgs):
+    """ Custom callback to track controller parameters during test episodes """
+    evalEpisodeRewards.append(localArgs["rewards"][0])
+    evalEpisodeObservations.append(localArgs["observations"][0])
+
+
+mean_reward, std_reward = evaluate_policy(model, model.get_env(), n_eval_episodes=1, callback=evaluationCallback)
+
+
---
+++ app/MTExperiments/TrainingScripts/TrainingScriptBlueprint.py
@@ -0,0 +1,190 @@
+
+docker exec -it ros_ml_container1 bash
+
+source ~/myenv/bin/activate
+source /opt/ros/noetic/setup.bash
+source /catkin_ws/devel/setup.bash
+
+python
+
+import subprocess
+import os
+import sys
+import time
+sys.path.append("/catkin_ws/src/jointcontrol/scripts")
+
+import numpy as np
+
+""" 
+Control script for experiment 4 
+
+DQN, DDPG, PPO and Random Agents trained to perform step responses from 0 to -1.57 rad
+
+Plots include average return of training and separate testepisodes
+
+Logs are stored in /app/4logs
+
+
+CONFIG BLUEPRINT
+
+config = {
+    "jointID": 0,
+    "logdir": "/app/logExperiment4",
+    "modelclass": "DQN",
+    "modelrun": 0,
+    "modelname": "DQN",
+    "learningRate": 0.001,
+    "trainingTimesteps": 10000,
+    "policyNetwork": [{'pi': [32, 32], 'vf': [32, 32]}],
+    "optimizer": "th.nn.tanh",
+    "discretisation": 0.5,
+    "resetConfig": str({ "initialPos":0, "stepPos":-1.57, "samplesPerStep":150, "maxSteps":40 }),
+    "trajectoryType": "step"
+}
+
+
+# PPO & DDPG type policy args
+policy_kwargs = dict(
+    activation_fn = th.nn.Tanh,
+    net_arch=[ dict(pi=[ 32,32 ], vf=[ 32,32 ]) ]
+)
+
+# DQN type policy args
+policy_kwargs = dict(
+    activation_fn = th.nn.Tanh,
+    net_arch=[ 32, 32, 32 ]
+)
+
+"""
+
+runTraining = True      # Determines, whether or not training is conducted or only visualisation is performed
+trainingSteps = 15000   # Determines performed training steps
+
+# Start tensorboard to allow for visualisation of training process
+logdir = "/app/MTExperiments/Data/4StepResponseTrainingDefaultParams"
+os.system("tensorboard --logdir {}/tensorboard --host 0.0.0.0 --port 6006 &".format(logdir))
+
+# Point to RL agent deployment script
+agentPath = "/app/MTExperiments/TrainingScripts/RLAgent.py"
+
+
+#############################################################################
+## DQN Discrete
+
+DQNConfig = {
+    "jointID": 0,
+    "logdir": logdir,
+    "modelclass": "DQN",
+    "modelrun": 0,
+    "modelname": "DQN",
+    "learningRate": None, 
+    "trainingTimesteps": trainingSteps,
+    "policyNetwork": None,
+    "optimizer": None,
+    "discretisation": 0.1,
+    "resetConfig": { "initialPos":-1.57, "stepPos":0, "samplesPerStep":150, "maxSteps":40 },
+    "trajectoryType": "step"
+}
+
+
+#############################################################################
+## DDPG Continuous
+
+DDPGConfig = {
+    "jointID": 0,
+    "logdir": logdir,
+    "modelclass": "DDPG",
+    "modelrun": 0,
+    "modelname": "DDPG",
+    "learningRate": None, 
+    "trainingTimesteps": trainingSteps,
+    "policyNetwork": None,
+    "optimizer": None,
+    "discretisation": None,
+    "resetConfig": { "initialPos":-1.57, "stepPos":0, "samplesPerStep":150, "maxSteps":40 },
+    "trajectoryType": "step"
+}
+
+#############################################################################
+## PPO Discrete
+
+PPODiscreteConfig = {
+    "jointID": 0,
+    "logdir": logdir,
+    "modelclass": "PPO",
+    "modelrun": 0,
+    "modelname": "PPO_Discrete",
+    "learningRate": None, 
+    "trainingTimesteps": trainingSteps,
+    "policyNetwork": None,
+    "optimizer": None,
+    "discretisation": 0.1,
+    "resetConfig": { "initialPos":-1.57, "stepPos":0, "samplesPerStep":150, "maxSteps":40 },
+    "trajectoryType": "step"
+}
+
+#############################################################################
+## PPO Continuous
+
+PPOContinuousConfig = {
+    "jointID": 0,
+    "logdir": logdir,
+    "modelclass": "PPO",
+    "modelrun": 0,
+    "modelname": "PPOContinuous",
+    "learningRate": None, 
+    "trainingTimesteps": trainingSteps,
+    "policyNetwork": None,
+    "optimizer": None,
+    "discretisation": None,
+    "resetConfig": { "initialPos":-1.57, "stepPos":0, "samplesPerStep":150, "maxSteps":40 },
+    "trajectoryType": "step"
+}
+
+
+# Debug: for one environment
+#p = subprocess.Popen([sys.executable, agentPath, str(PPOContinuousConfig)])
+#streamdata = p.communicate()[0]
+#print(p.returncode)
+
+#############################################################################
+## RUN Training Episodes for each Algorithm
+
+def runAgentTests(config, testRuns=5):
+    """ Performs testRuns """
+    processes = [None for _ in range(testRuns)]
+    #
+    for i in range(testRuns):
+        # Start process in the background
+        config["jointID"] = i
+        config["modelrun"] = i
+        processes[i] = subprocess.Popen([sys.executable, agentPath, str(config)])
+        # Wait for env to register
+        time.sleep(1)
+    #
+    # Wait for processes to finish and record return codes
+    rc = []
+    for i in range(testRuns):
+        streamdata = processes[i].communicate()[0]
+        rc.append(processes[i].returncode)
+    #
+    print("{} agent tests successfully exited with return codes {}".format(config["modelname"], rc))
+
+
+#############################################################################
+## VISUALISE training results
+
+# Load numpy results
+def loadTestepisodeResults(config, testRuns=5):
+    """ Loads evaluation episode results based on configuration """
+    ret = []
+    for run in range(testRuns):
+        filename = "{}/testepisodes_{}_{}.npy".format(config["logdir"], config["modelname"], run)
+        ret.append(np.load(filename), allow_pickle=True).item()
+    #
+    return ret
+
+
+# 
+#testepisodeResults = np.load('/app/MTExperiments/Data/4StepResponseTrainingDefaultParams/testepisodes_DQN_0.npy', allow_pickle=True).item()
+
